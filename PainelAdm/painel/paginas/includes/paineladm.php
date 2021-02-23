519a2b857d6a0e3838b5294f8d30f549cff47f0a
gebso <gebso@DESKTOP-A6O5F20>
2021-02-23 10:22:34 -0300

    att

--- PainelAdm/painel/paginas/includes/paineladm.php
+++ PainelAdm/painel/paginas/includes/paineladm.php
@@ -138,7 +138,15 @@
 
 <!--INICIO FOOTER-->
 
-   
+     <footer class="main-footer">
+
+                <strong>Copyright &copy;2021 <a href="https://adminlte.io">Geb.io</a>.</strong>
+                All rights reserved.
+                <div class="float-right d-none d-sm-inline-block">
+                    <b>Version</b> 3.1.0-rc
+                </div>
+            </footer>
+
 
             <!-- Control Sidebar -->
             <aside class="control-sidebar control-sidebar-dark">
